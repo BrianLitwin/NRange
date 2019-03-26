43d37ff3bdf0aeda3dddb7a15b2ce63800d6fe14
Brian Litwin <brian.n.litwin@gmail.com>
2019-03-26 14:31:37 -0400

    Update model.test.js

--- app/src/model/model.test.js
+++ app/src/model/model.test.js
@@ -4,7 +4,29 @@ import { getWordsInNumber } from "./helperFunctions";
 describe("Calculating the number of letters in a number-string", () => {
   describe("getLetterCount", () => {
     
-    const manualTest = (n) = > {
+    const manualTest = (n) => {
+       const incrementMap = (map, char) => {
+          const hasChar = map.get(char);
+          if (hasChar) {
+            map.set(char, map.get(char) + 1);
+          } else {
+            map.set(char, 1);
+          }
+        };
+
+        for (var i = 1; i <= n; i++) {
+          var words = getWordsInNumber(i).nArray;
+          for (var wordSubArray of wordSubArray) {
+            for (var word of wordArray) {
+              for (var j = 0; j < word.length; j++) {
+                const char = word.charAt(j);
+                increment(charMap, char);
+              }
+            }
+          }
+        }
+        return charMap;
+      }
     }
     
     manualCheck(n) {
