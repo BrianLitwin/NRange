9c55a3de2f06c5c1b1f342dbb090b1d50ee1cf63
Brian Litwin <brian.n.litwin@gmail.com>
2019-03-26 11:03:23 -0400

    Update helperFunctions.test.js

--- app/src/model/helperFunctions.test.js
+++ app/src/model/helperFunctions.test.js
@@ -1,7 +1,7 @@
 import { makeNumberArray } from "./helperFunctions";
 
 describe("helper functions", () => {
-  it("converting numbers into arrays", () => {
+  it("converts numbers into arrays", () => {
     expect(makeNumberArray(1)).toEqual([1]);
     expect(makeNumberArray(10)).toEqual([1, 0]);
     expect(makeNumberArray(299)).toEqual([2, 9, 9]);
@@ -11,6 +11,14 @@ describe("helper functions", () => {
     expect(makeNumberArray(1234567)).toEqual([1, 2, 3, 4, 5, 6, 7]);
     expect(makeNumberArray(14234567)).toEqual([1, 4, 2, 3, 4, 5, 6, 7]);
   });
+  
+  it("converts number arrays into string arrays", () => {
+    expect(makeNumberArray(1)).toBe(["one"])
+    expect(makeNumberArray(15)).toBe(["fifteen")])
+    expect(makeNumberArray(72)).toBe(["seventy", "two"])
+    expect(makeNumberArray(123)).toBe(["one", "hundred", "twenty", "three"])
+    expect(makeNumberArray(123456)).toBe(["one", "hundred", "twenty", "three", "thousand", "four", "hundred", "fifty", "six"])
+    
+  })
 
-  describe("getting array of words from number", () => {});
 });
