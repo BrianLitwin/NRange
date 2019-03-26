8b1803a8e7a2c76e093e045fc8506507f4586a64
Brian Litwin <brian.n.litwin@gmail.com>
2019-03-26 14:09:01 -0400

    Update model.test.js

--- app/src/model/model.test.js
+++ app/src/model/model.test.js
@@ -1,7 +1,12 @@
 import { getLetterCount } from "./model";
+import { getWordsInNumber } " from "./helperFunctions";
 
 describe("Calculating the number of letters in a number-string", () => {
   describe("getLetterCount", () => {
+    
+    const manualTest = (n) = > {
+    }
+    
     manualCheck(n) {
       
     }
